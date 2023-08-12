84ca7695aa7979a950a5a21a2e980593c6869b1c
bdoobd <G3xAg0N@mail.ru>
2023-08-12 15:52:12 +0300

    STUCK: Косяк с фильтром

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from binance.client import Client
-import keys
-import pandas as pd
-import time
+# from binance.client import Client
+# import keys
+# import pandas as pd
+# import time
 import requests
 import json
 
@@ -23,25 +23,17 @@ def get_symbols(raw_data):
     return serialized['symbols']
 
 
-print(get_symbols(handle_json(uri)))
+# formatted_string = json.dumps(get_symbols(handle_json(uri)), indent=2)
+# print(type(formatted_string))
 
 
-# print(handle_json(uri))
-
-# formatted = json.dumps(response, indent=2)
-# formatted = json.loads(data)
-
-# output = json.dumps(formatted)
-
-# print(formatted['symbols'][]['symbol'])
-# print(type(formatted))
-
-# for key in formatted['symbols']:
-# output = ''
-# if 'USDT' in key['symbol']:
-#     output = key['symbol']
-#     for filter in key['filters']:
-#         if 'LOT_SIZE' in (filter['filterType']):
-# output = output + ' LOT_SIZE is ' + filter['stepSize']
-# print(filter['stepSize'])
-# print(output)
+for key in get_symbols(handle_json(uri)):
+    # TODO:  Нужен coin в названии которого есть USDT
+    if 'USDT' in key['symbol']:
+        # TODO: В symbol есть ключ filter который содержит массив фильтров
+        for filter in key['filters']:
+            # TODO: Нужный тип фильтра LOT_SIZE
+            if 'LOT_SIZE' in filter['filterType']:
+                print(str(key['symbol']) + ' => ' + json.dumps(filter))
+            # print(filter['filterType'])
+            # TODO: Нашёл почему дублиуются coins, кроме фильтра LOT_SIZE есть фильтр MARKET_LOT_SIZE. Как сделать более строгий выбор фильтра? Что то типа filterType === 'LOT_SIZE'.
