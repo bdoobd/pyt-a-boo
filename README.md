5d95e81dcb6aa6e9b275c31f336fc2598f8e2b13
bdoobd <G3xAg0N@mail.ru>
2023-08-20 12:29:00 +0300

    Торговля настроена на тестовую сеть

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Jiiiiihuuu!
+
+Test Network Spot: https://testnet.binance.vision/
+Manulas: https://www.binance.com/en/support/faq/how-to-test-my-functions-on-binance-testnet-ab78f9a1b8824cf0a106b4229c76496d
--- main.py
+++ main.py
@@ -1,43 +1,63 @@
 from binance.client import Client
-import keys
+import test_keys
 import pandas as pd
 import time
 import helper
 import get_symbols
 
-# import json
+import json
 
 coin_list = get_symbols.get_symbols()
 
-client = Client(keys.api_key, keys.secret_key, testnet=True)
+client = Client(test_keys.api_key, test_keys.secret_key, testnet=True)
 
-# print(client.get_account())
+nice_print = json.dumps(client.get_account(), indent=4)
+
+print(nice_print)
+
+"""
+Функция поиска монеты USDT которая показывает максимальный рост на момент запуска функции
+
+return string Название монетеы
+"""
 
 
 def top_coin():
+    # Выбрать все тикеры для всех монет
     all_tickers = pd.DataFrame(client.get_ticker())
+    # Отфильтровать тикеры в названиях которых есть USDT
     usdt = all_tickers[all_tickers.symbol.str.contains("USDT")]
+    # Отфильтровать тикеры, удалить строки в названии которых есть строки UP и DOWN
     work = usdt[
         ~((usdt.symbol.str.contains("UP")) | (usdt.symbol.str.contains("DOWN")))
     ]
+    # Выбрать строку из фрейма в котором столбец priceChangePercent имеет максимальное значение
     top_coin = work[work.priceChangePercent == work.priceChangePercent.max()]
+    # Из отфильтрованной строки выбрать первую колонку с названием монеты
     top_coin = top_coin.symbol.values[0]
 
     return top_coin
 
 
 def get_last_data(symbol, period, interval):
+    # Получить исторические данные для монеты за заданный период
     data = pd.DataFrame(client.get_historical_klines(
         symbol, period, interval + 'min ago UTC'))
+    # Выбрать все строки и столбцы с первого по шестой (индекс с 0 по 5)
     data = data.iloc[:, :6]
+    # Задать заголовки этим столбцам
     data.columns = ['Time', 'Open', 'High', 'Low', 'Close', 'Volume']
+    # Проиндексировать столбец Time, типа столбца для сортировки
     data = data.set_index('Time')
+    # Конвертирует дату в милисекундах в обычное отображение даты
     data.index = pd.to_datetime(data.index, unit='ms')
+    # Конвертирует содержимое ячеек таблицы в число с плавающей точкой
     data = data.astype('float')
 
     return data
 
 
+# Степень точности указания стосмости монеты
 coin_lot_size = helper.get_lot_size(top_coin(), coin_list)
 
 
@@ -47,7 +67,7 @@ def run(amount, lower_limit=0.985, upper_limit=1.02, trade_open=False):
         data_grid = get_last_data(
             coin, client.KLINE_INTERVAL_1MINUTE, '120')
     except:
-        print('BUY cycle check error, restating after 1 minute')
+        print('Ошибка процесса покупки монеты, перезапуск через одну минуту')
         time.sleep(61)
         coin = top_coin()
         data_grid = get_last_data(
@@ -58,27 +78,26 @@ def run(amount, lower_limit=0.985, upper_limit=1.02, trade_open=False):
 
     if (data_grid.Close.pct_change() + 1).cumprod().iloc[-1] > 1:
 
-        print('<**** Growing Coin found ****>')
-        print(f'TOP COIN: ' + str(coin))
-        print(f'PRICE: ' + str(data_grid.Close.iloc[-1]))
-        print(f'AVAILABLE QTY: ' + str(quantity))
+        # print('<**** Growing Coin found ****>')
+        print('<**** Найдена растушая монета ****>')
+        print(f'Тип монеты: ' + str(coin))
+        print(f'Стоимость: ' + str(data_grid.Close.iloc[-1]))
+        print(f'Можно купить: ' + str(quantity))
 
         try:
-            # order = client.create_test_order(
-            #     symbol=coin,
-            #     side=client.SIDE_BUY,
-            #     type=client.ORDER_TYPE_MARKET,
-            #     quantity=quantity)
-
-            order = True
+            order = client.create_order(
+                symbol=coin,
+                side=client.SIDE_BUY,
+                type=client.ORDER_TYPE_MARKET,
+                quantity=quantity)
 
             if order:
-                print('Nice shopping!')
-                print(order)
+                print('<**** Удачная покупка ****>')
+                nice_order = json.dumps(order, indent=4)
+                print(nice_order)
 
-            # FIXME: Не уверен, что стоимость монеты та же, что и табличке
-            coin_price = data_grid.Close.iloc[-1]
-            # coin_price = float(order["fills"][0]["price"])
+            coin_price = float(order["fills"][0]["price"])
+            have_quantity = float(order['fills'][0]['qty'])
 
             trade_open = True
 
@@ -92,41 +111,43 @@ def run(amount, lower_limit=0.985, upper_limit=1.02, trade_open=False):
                     data_grid = get_last_data(
                         coin, client.KLINE_INTERVAL_1MINUTE, '2')
 
-                print('<**** SELL cycle check ****>')
-                print(f'UPPER LIMIT: ' + str(coin_price * upper_limit))
-                print(f'PRICE BOUGHT: ' + str(coin_price))
-                print(f'GRID PRICE: ' + str(data_grid.Close[-1]))
-                print(f'LOWER LIMIT: ' + str(coin_price * lower_limit))
+                print('<**** Анализ роста / падения монеты ****>')
+                print(f'Верхний лимит продажи: ' +
+                      str(coin_price * upper_limit))
+                print(f'Стоимость покупки: ' + str(coin_price))
+                print(f'Торгуется сейчас: ' + str(data_grid.Close[-1]))
+                print(f'Нижний лимит продажи: ' +
+                      str(coin_price * lower_limit))
                 # print(data_grid)
 
                 if data_grid.Close[-1] <= coin_price * lower_limit or data_grid.Close[-1] >= coin_price * upper_limit:
-                    print('Time to sell coin')
-                    print(data_grid.Close[-1])
+                    print('<**** Время продавать монету ****>')
+                    # print(data_grid.Close[-1])
                     try:
-                        # order = client.create_test_order(
-                        #     symbol=coin,
-                        #     side=client.SIDE_SELL,
-                        #     type=client.ORDER_TYPE_MARKET,
-                        #     quantity=quantity
-                        # )
+                        order = client.create_order(
+                            symbol=coin,
+                            side=client.SIDE_SELL,
+                            type=client.ORDER_TYPE_MARKET,
+                            quantity=have_quantity
+                            # quantity=quantity
+                        )
 
-                        # print(f'SELL oreder ready as:')
-                        # print(order)
-
-                        order = True
+                        print('<**** Монета продана со следующими данными ****')
+                        nice_sell_order = json.dumps(order, indent=4)
+                        print(nice_sell_order)
                     except Exception as sell_error:
-                        print('Error placing SELL order')
-                        print(f'Sell error: ' + str(sell_error))
+                        print('Ошибка заказа на продажу монеты')
+                        print(f'Описание: ' + str(sell_error))
 
                     time.sleep(5)
                     break
 
         except Exception as err:
-            print('Error placing BUY order')
-            print(f'Error:' + str(err))
+            print('Ошибка заказа покупки монеты')
+            print(f'Описание:' + str(err))
 
     else:
-        print('<=== No grows, no buy ===>')
+        print('<=== Не найден ни один актуальный вариант, ждёмс ===>')
         time.sleep(10)
 
 
